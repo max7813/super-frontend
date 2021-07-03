f9f300dac04b7a4c0f1a5f036d5edc2b297af209
max7813 <maksmiks78@mail.ru>
2021-07-03 13:25:17 +0500

    change

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-const fs = require('fs');
+/*
+const sections = [
+    ['login', 'Окно логина'],
+    ['profile', 'Мой профиль'],
+    ['about', 'Об игре'],
+];
 
-const text = fs.readFileSync('script.js', 'utf8');
-console.log(text);
+const application = document.querySelector('#application');
+const nav = document.getElementById('navigation');
+
+for (let section of sections) {
+    const button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-section', section[0]);
+    button.value = section[1];
+    nav.appendChild(button);
+}
+
+const liveSectionsCollection = application.getElementsByTagName('section');
+
+nav.addEventListener('click', function(event) {
+    const sectionId = event.target.getAttribute('data-section');
+    console.log(sectionId);
+
+    const liveSectionsArray = Array.from(liveSectionsCollection);
+
+    liveSectionsArray.forEach(function(sectionElement) {
+        sectionElement.hidden = true;
+        if (sectionElement.id === sectionId) {
+            sectionElement.hidden = false;
+        }
+    });
+});
+*/
+const greeting = document.getElementsByClassName('greeting')[0];
+
+greeting.addEventListener('click', () => {
+    greeting.classList.add('hidden');
+}, false);
